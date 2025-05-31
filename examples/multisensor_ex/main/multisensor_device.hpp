c9234bb33a3fbcae61001038ab508c1bc43f529a
Ozan Oner <ozan.oner@gmail.com>
2025-05-31 16:45:46 +0000

    Update read functions

--- examples/multisensor_ex/main/multisensor_device.hpp
+++ examples/multisensor_ex/main/multisensor_device.hpp
@@ -11,28 +11,86 @@
 #include "bh1750.h"
 #include "bme280.h"
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "led_indicator.h"
 #include "sensgreen.hpp"
 
 namespace app
 {
 
-class TemperatureSensorBrandA : public sensgreen::device::SensorBase<sensgreen::device::TemperatureMetric>
+class Bme280
+    : public sensgreen::device::SensorBase<sensgreen::device::TemperatureMetric, sensgreen::device::HumidityMetric,
+                                           sensgreen::device::PressureMetric>
 {
+   private:
+    bme280_handle_t m_bme280;
+
    public:
     virtual int read() override
     {
+        esp_err_t err = ESP_OK;
         // first read temperature from the physical sensor
         // then update the metric value
         get<sensgreen::device::TemperatureMetric>().setValue(25.0);
 
-        return 0;
+        float temperature = 0.0;
+        float humidity    = 0.0;
+        float pressure    = 0.0;
+
+        vTaskDelay(300 / portTICK_RATE_MS);
+        err = bme280_read_temperature(m_bme280, &temperature);
+        if (ESP_OK == err)
+        {
+            vTaskDelay(300 / portTICK_RATE_MS);
+            err = bme280_read_humidity(m_bme280, &humidity);
+        }
+        if (ESP_OK == err)
+        {
+            vTaskDelay(300 / portTICK_RATE_MS);
+            err = bme280_read_pressure(m_bme280, &pressure);
+        }
+        if (ESP_OK == err)
+        {
+            get<sensgreen::device::TemperatureMetric>().setValue(temperature);
+            get<sensgreen::device::HumidityMetric>().setValue(humidity);
+            get<sensgreen::device::PressureMetric>().setValue(pressure);
+        }
+
+        return (int)err;
     }
+
+    void setHandle(const bme280_handle_t handle) { m_bme280 = handle; }
 };
 
-class MyDevice : public sensgreen::device::esp32::Esp32Device<TemperatureSensorBrandA>
+class Bh1750 : public sensgreen::device::SensorBase<sensgreen::device::LightMetric>
 {
-    using sensgreen::device::esp32::Esp32Device<TemperatureSensorBrandA>::Esp32Device;  // inherit constructors
+   private:
+    bh1750_handle_t m_bh1750;
+
+   public:
+    virtual int read() override
+    {
+        esp_err_t err = ESP_OK;
+        // first read light from the physical sensor
+        // then update the metric value
+        get<sensgreen::device::LightMetric>().setValue(100.0);
+
+        bh1750_power_on(bh1750);
+        cmd_measure = BH1750_ONETIME_4LX_RES;
+        bh1750_set_measure_mode(bh1750, cmd_measure);
+        vTaskDelay(30 / portTICK_RATE_MS);
+        err = bh1750_get_data(bh1750, &bh1750_data);
+
+        return (int)err;
+    }
+
+    void setHandle(const bh1750_handle_t handle) { m_bh1750 = handle; }
+};
+
+class MyDevice : public sensgreen::device::esp32::Esp32Device<Bme280, Bh1750>
+{
+    using sensgreen::device::esp32::Esp32Device<Bme280, Bh1750>::Esp32Device;  // inherit constructors
 
    private:
     static constexpr int        DEVICE_LED_GPIO    = 8;
@@ -73,10 +131,14 @@ class MyDevice : public sensgreen::device::esp32::Esp32Device<TemperatureSensorB
 
         m_i2cBus = i2c_bus_create(I2C_MASTER_NUM, &DEVICE_I2C_CONFIG);
         assert(nullptr != m_i2cBus);
+
         m_bh1750 = bh1750_create(m_i2cBus, BH1750_I2C_ADDRESS_DEFAULT);
         assert(nullptr != m_bh1750);
+        getSensor<Bh1750>().setHandle(m_bh1750);
+
         m_bme280 = bme280_create(m_i2cBus, BME280_I2C_ADDRESS_DEFAULT);
         assert(nullptr != m_bme280);
+        getSensor<Bme280>().setHandle(m_bme280);
 
         return err;
     }
