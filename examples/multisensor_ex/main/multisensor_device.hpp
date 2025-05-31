6fd06735cb5a1ee7a5835308a8f65a1719daa05d
Ozan Oner <ozan.oner@gmail.com>
2025-05-31 14:01:58 +0000

    Add bme280 & bh1750

--- examples/multisensor_ex/main/multisensor_device.hpp
+++ examples/multisensor_ex/main/multisensor_device.hpp
@@ -8,6 +8,8 @@
 
 #include <cassert>
 
+#include "bh1750.h"
+#include "bme280.h"
 #include "esp_err.h"
 #include "led_indicator.h"
 #include "sensgreen.hpp"
@@ -33,7 +35,11 @@ class MyDevice : public sensgreen::device::esp32::Esp32Device<TemperatureSensorB
     using sensgreen::device::esp32::Esp32Device<TemperatureSensorBrandA>::Esp32Device;  // inherit constructors
 
    private:
-    static constexpr int DEVICE_LED_GPIO = 8;
+    static constexpr int        DEVICE_LED_GPIO    = 8;
+    static constexpr int        I2C_MASTER_SCL_IO  = 22;
+    static constexpr int        I2C_MASTER_SDA_IO  = 21;
+    static constexpr i2c_port_t I2C_MASTER_NUM     = I2C_NUM_0;
+    static constexpr int        I2C_MASTER_FREQ_HZ = (100 * 1000);
 
     static constexpr led_indicator_strips_config_t DEVICE_LED_CONFIG {
         {DEVICE_LED_GPIO, 1, LED_PIXEL_FORMAT_GRB, LED_MODEL_WS2812, {false}},
@@ -42,6 +48,13 @@ class MyDevice : public sensgreen::device::esp32::Esp32Device<TemperatureSensorB
 
     led_indicator_config_t m_ledIndconfig;
     led_indicator_handle_t m_led;
+
+    static constexpr i2c_config_t DEVICE_I2C_CONFIG {
+        I2C_MODE_MASTER, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO, true, true, {I2C_MASTER_FREQ_HZ}, 0};
+
+    i2c_bus_handle_t m_i2cBus = NULL;
+    bh1750_handle_t  m_bh1750 = NULL;
+    bme280_handle_t  m_bme280 = NULL;
 
    public:
     esp_err_t boardInit()
@@ -57,6 +70,13 @@ class MyDevice : public sensgreen::device::esp32::Esp32Device<TemperatureSensorB
         m_led = led_indicator_create(&m_ledIndconfig);
         assert(nullptr != m_led);
         err = led_indicator_set_on_off(m_led, false);
+
+        m_i2cBus = i2c_bus_create(I2C_MASTER_NUM, &DEVICE_I2C_CONFIG);
+        assert(nullptr != m_i2cBus);
+        m_bh1750 = bh1750_create(m_i2cBus, BH1750_I2C_ADDRESS_DEFAULT);
+        assert(nullptr != m_bh1750);
+        m_bme280 = bme280_create(m_i2cBus, BME280_I2C_ADDRESS_DEFAULT);
+        assert(nullptr != m_bme280);
 
         return err;
     }
