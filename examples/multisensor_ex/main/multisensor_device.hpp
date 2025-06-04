a3a1054eb8befa7aa9b27996d849eb7e1f9bea82
Ozan Oner <ozan.oner@gmail.com>
2025-06-04 14:35:47 +0100

    Develop a multisensor device (#34)
    * working example
    
    * integrate esp-iot-solution for sensors
    
    * Add bme280 & bh1750
    
    * Update read functions
    
    * Update sensor init & read
    
    * light working
    
    * bme280 updated
    
    * Add readme & refactor
    
    * Add app config

---
+++ examples/multisensor_ex/main/multisensor_device.hpp
@@ -0,0 +1,163 @@
+/**
+ * @file multisensor_device.hpp
+ * @brief Multisensor device definition
+ * @version 0.1
+ * @date 2025-05-30
+ * @copyright Copyright (c) 2025
+ */
+
+#pragma once
+
+#include <cassert>
+#include <string_view>
+
+#include "app_utils.hpp"
+#include "bh1750.h"
+#include "bme280.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led_indicator.h"
+#include "sensgreen.hpp"
+
+namespace app
+{
+
+class Bme280
+    : public sensgreen::device::SensorBase<sensgreen::device::TemperatureMetric, sensgreen::device::HumidityMetric,
+                                           sensgreen::device::PressureMetric>
+{
+   private:
+    bme280_handle_t       m_bme280;
+    static constexpr auto TAG = "bme280";
+
+   public:
+    int init(void *param) override
+    {
+        i2c_bus_handle_t i2cBus = static_cast<i2c_bus_handle_t>(param);
+        m_bme280                = bme280_create(i2cBus, BME280_I2C_ADDRESS_DEFAULT);
+        assert(nullptr != m_bme280);
+
+        esp_err_t err = bme280_default_init(m_bme280);
+        PRINT_IF_ERR(err);
+        return (int)err;
+    }
+
+    int read() override
+    {
+        float     data = 0.0;
+        esp_err_t err  = ESP_OK;
+
+        vTaskDelay(pdMS_TO_TICKS(300));
+        PRINT_LOC_D("before bme280_read_temperature");
+        err = bme280_read_temperature(m_bme280, &data);
+        PRINT_IF_ERR(err);
+        RETURN_IF_ERR(err);
+        get<sensgreen::device::TemperatureMetric>().setValue(data);
+
+        PRINT_LOC_D("before bme280_read_humidity");
+        err = bme280_read_humidity(m_bme280, &data);
+        PRINT_IF_ERR(err);
+        RETURN_IF_ERR(err);
+        get<sensgreen::device::HumidityMetric>().setValue(data);
+
+        PRINT_LOC_D("before bme280_read_pressure");
+        err = bme280_read_pressure(m_bme280, &data);
+        PRINT_IF_ERR(err);
+        RETURN_IF_ERR(err);
+        get<sensgreen::device::PressureMetric>().setValue(data);
+
+        return (int)err;
+    }
+};
+
+class Bh1750 : public sensgreen::device::SensorBase<sensgreen::device::LightMetric>
+{
+   private:
+    bh1750_handle_t       m_bh1750;
+    static constexpr auto TAG = "bh1750";
+
+   public:
+    int init(void *param) override
+    {
+        i2c_bus_handle_t i2cBus = static_cast<i2c_bus_handle_t>(param);
+        m_bh1750                = bh1750_create(i2cBus, BH1750_I2C_ADDRESS_DEFAULT);
+        assert(nullptr != m_bh1750);
+        return (int)ESP_OK;
+    }
+
+    int read() override
+    {
+        float     data = 0.0;
+        esp_err_t err  = ESP_OK;
+
+        PRINT_LOC_D("before bh1750_power_on");
+        bh1750_power_on(m_bh1750);
+        bh1750_set_measure_mode(m_bh1750, BH1750_ONETIME_4LX_RES);
+
+        vTaskDelay(pdMS_TO_TICKS(30));
+        PRINT_LOC_D("before bh1750_get_data");
+        err = bh1750_get_data(m_bh1750, &data);
+        PRINT_IF_ERR(err);
+        RETURN_IF_ERR(err);
+
+        get<sensgreen::device::LightMetric>().setValue(data);
+
+        return (int)err;
+    }
+};
+
+class MyDevice : public sensgreen::device::esp32::Esp32Device<Bme280, Bh1750>
+{
+    using sensgreen::device::esp32::Esp32Device<Bme280, Bh1750>::Esp32Device;  // inherit constructors
+
+   private:
+    static constexpr auto TAG = "device";
+
+    static constexpr int        DEVICE_LED_GPIO    = 8;
+    static constexpr int        I2C_MASTER_SDA_IO  = 21;
+    static constexpr int        I2C_MASTER_SCL_IO  = 22;
+    static constexpr i2c_port_t I2C_MASTER_NUM     = I2C_NUM_0;
+    static constexpr int        I2C_MASTER_FREQ_HZ = (100 * 1000);
+
+    static constexpr led_indicator_strips_config_t DEVICE_LED_CONFIG {
+        {DEVICE_LED_GPIO, 1, LED_PIXEL_FORMAT_GRB, LED_MODEL_WS2812, {false}},
+        LED_STRIP_RMT,
+        {RMT_CLK_SRC_DEFAULT, (10 * 1000 * 1000), 0, {false}}};
+
+    led_indicator_config_t m_ledConfig;
+    led_indicator_handle_t m_led;
+
+    static constexpr i2c_config_t DEVICE_I2C_CONFIG {
+        I2C_MODE_MASTER, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO, true, true, {I2C_MASTER_FREQ_HZ}, 0};
+
+    i2c_bus_handle_t m_i2cBus = NULL;
+
+   public:
+    int init(void *param = nullptr) override
+    {
+        esp_err_t err = ESP_OK;
+
+        // LED initialisation
+        m_ledConfig.mode                        = LED_STRIPS_MODE;
+        m_ledConfig.led_indicator_strips_config = const_cast<led_indicator_strips_config_t *>(&DEVICE_LED_CONFIG);
+        m_ledConfig.blink_lists                 = nullptr;  // define blinks later if needed
+        m_ledConfig.blink_list_num              = 0;
+
+        m_led = led_indicator_create(&m_ledConfig);
+        assert(nullptr != m_led);
+        err = led_indicator_set_on_off(m_led, false);
+        RETURN_IF_ERR(err);
+
+        m_i2cBus = i2c_bus_create(I2C_MASTER_NUM, &DEVICE_I2C_CONFIG);
+        assert(nullptr != m_i2cBus);
+
+        err = getSensor<Bh1750>().init(m_i2cBus);
+        RETURN_IF_ERR(err);
+
+        err = getSensor<Bme280>().init(m_i2cBus);
+
+        return (int)err;
+    }
+};
+}  // namespace app
---
+++ examples/multisensor_ex/main/multisensor_ex.cpp
@@ -0,0 +1,128 @@
+/**
+ * @file multisensor_ex.cpp
+ * @brief Multisensor example
+ * @version 0.1
+ * @date 2025-05-30
+ * @copyright Copyright (c) 2025
+ */
+
+#include <string_view>
+
+#include "app_utils.hpp"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif_sntp.h"
+#include "esp_sntp.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "lwip/ip_addr.h"
+#include "multisensor_device.hpp"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "protocol_examples_utils.h"
+#include "sdkconfig.h"
+#include "sensgreen.hpp"
+
+using sensgreen::device::DeviceConfig;
+using sensgreen::mqtt::ConnConfig;
+using sensgreen::mqtt::MqttEvent;
+using sensgreen::mqtt::esp32::Esp32MqttConnector;
+
+namespace
+{
+constexpr auto TAG = "app";
+
+DeviceConfig  deviceConfig {CONFIG_DEVICE_UID, CONFIG_MQTT_TOPIC, CONFIG_MQTT_TOPIC, CONFIG_MQTT_TOPIC,
+                           CONFIG_MQTT_TOPIC};
+app::MyDevice device {deviceConfig};
+auto&         connector = Esp32MqttConnector::instance();
+TaskHandle_t  publishTaskHandle {nullptr};
+
+constexpr std::string_view NTP_SERVER {"pool.ntp.org"};
+
+void timeSynced(struct timeval* tv)
+{
+    esp_err_t err = connector.connect();
+    PRINT_IF_ERR(err);
+    PRINT_IF_SUCC(err, "mqtt-client started");
+}
+
+void publishTask(void* pvParameters)
+{
+    uint32_t periodMs = CONFIG_MQTT_PUBLISH_PERIOD_MINUTES * 60ul * 1000;
+    while (true)
+    {
+        device.readAllSensors();
+
+        const auto& report = device.report();
+        auto        jstr   = report.dump();
+        PRINT_LOC(jstr.c_str());
+
+        esp_err_t err = (esp_err_t)connector.publish(deviceConfig.topicData, jstr);
+        PRINT_IF_ERR(err);
+
+        vTaskDelay(pdMS_TO_TICKS(periodMs));  // Delay for a minute
+    }
+}
+
+}  // namespace
+
+extern "C" void app_main(void)
+{
+    // various initialisations
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // initialise SNTP
+    esp_sntp_config_t sntpConfig          = ESP_NETIF_SNTP_DEFAULT_CONFIG(NTP_SERVER.data());
+    sntpConfig.start                      = false;
+    sntpConfig.server_from_dhcp           = true;
+    sntpConfig.renew_servers_after_new_IP = true;
+    sntpConfig.index_of_first_server      = 1;
+    sntpConfig.ip_event_to_renew          = IP_EVENT_STA_GOT_IP;
+    sntpConfig.sync_cb                    = timeSynced;
+    ESP_ERROR_CHECK(esp_netif_sntp_init(&sntpConfig));
+
+    // Initialise MQTT connector
+    ConnConfig config {CONFIG_MQTT_HOST, static_cast<int16_t>(std::stoi(CONFIG_MQTT_PORT)), CONFIG_MQTT_USER,
+                       CONFIG_MQTT_PASS};
+    ESP_ERROR_CHECK(connector.init(config));
+
+    // Start the publish task when MQTT is connected
+    connector.registerEventHandler(MqttEvent::CONNECTED,
+                                   [](const void*)
+                                   {
+                                       if (nullptr == publishTaskHandle)
+                                       {  // Create the FreeRTOS task
+                                           xTaskCreate(publishTask, "publish-task", 2048, nullptr, tskIDLE_PRIORITY + 1,
+                                                       &publishTaskHandle);
+                                       }
+                                       else
+                                       {
+                                           vTaskResume(publishTaskHandle);
+                                       }
+                                   });
+
+    // Stop the publish task when MQTT is disconnected
+    connector.registerEventHandler(MqttEvent::DISCONNECTED,
+                                   [](const void*)
+                                   {
+                                       if (nullptr != publishTaskHandle)
+                                       {
+                                           vTaskSuspend(publishTaskHandle);
+                                       }
+                                   });
+
+    // initialise device
+    ESP_ERROR_CHECK(device.init());
+
+    // connect wifi
+    ESP_ERROR_CHECK(example_connect());
+
+    // start SNTP
+    ESP_ERROR_CHECK(esp_netif_sntp_start());
+}
